08d9d6844bd086c71269ad6d9f0e6315af9c54dd
unknown <arcayned6075@gmail.com>
2025-04-30 11:21:26 -0400

    filtrado de facturas sin transaccion terminado solo cobros

--- contabilidad/api/facturas/filtrado_facturas.php
+++ contabilidad/api/facturas/filtrado_facturas.php
@@ -3,45 +3,73 @@ session_start();
 //require_once "db.php";
 require_once "../../db/db.php";
 class Filtrado_facturas extends DB{
-    public function listar_facturas_cobros_sin_transaccion($cobrado,$idcaja_bancos,$empresa) {
+    public function listar_facturas_cobros_sin_transaccion($cobrado,$cadena_cajaBancos,$empresa) {
       // $idempresa = $this->getidempresa($empresa);
                 // echo json_encode(array($cobrado,$idcaja_bancos,$empresa));
+                
+                ini_set('display_errors', 1);
+                ini_set('display_startup_errors', 1);
+                error_reporting(E_ALL);
+
 
         $lista = [];
         $array_idfacturas = [];
         $idempresa = $this->getidempresa($empresa);
     
+        // $array_cajaBancos = explode(",", $cadena_cajaBancos);
+        $array_cajaBancos = array_map('intval', explode(",", $cadena_cajaBancos));
+        $caja_bancos = implode(",", $array_cajaBancos);
         // Preparar la consulta
         // select * from factura where idorganizacion = 50 and cobrado != 0 and transacciones_idtransacciones = 0
-        
-        if($cobrado == 1){ //POR COBRAR
-            // $det_caja_banc = $this->dbc->query("SELECT * FROM detalle_caja_bancos_cobrar WHERE idcaja_bancos = '$idcaja_bancos' AND idfactura !=0"); //POR COBRAR
-            $det_caja_banc = $this->dbc->query("SELECT * FROM detalle_caja_bancos_cobrar WHERE idcaja_bancos IN('$idcaja_bancos') AND idfactura !=0"); //POR COBRAR
+        // echo json_encode(array($array_idfacturas,$array_cajaBancos,$empresa));
 
-            while ($factura_cajas = $this->dbc->fetch($det_caja_banc)) {
-                $factura_aux = $this->dbc->query("SELECT * FROM factura WHERE idfactura = '$factura_cajas[idfactura]'"); //POR COBRAR
-                $factu = $factura_aux->fetch_assoc();
-                if($factu['transacciones_idtransacciones'] == 0 && $factu['cobrado'] == 1 ){
-                    array_push($array_idfacturas, $factura_cajas['idfactura']);
-                }else{
+        $det_caja_banc = $this->dbc->query("SELECT * FROM detalle_caja_bancos_cobrar WHERE idcaja_bancos IN('$caja_bancos') AND idfactura !='0'"); //POR COBRAR
 
-                }
-                // array_push($lista, $factura_cajas['idfactura']);
+        while ($factura_cajas = $this->dbc->fetch($det_caja_banc)) {
+            $factura_aux = $this->dbc->query("SELECT * FROM factura WHERE idfactura = '$factura_cajas[idfactura]'"); //POR COBRAR
+            $factu = $factura_aux->fetch_assoc();
+            if($factu['transacciones_idtransacciones'] == 0 && $factu['cobrado'] == 1 ){
+                array_push($array_idfacturas, $factura_cajas['idfactura']);
+            }else{
+
             }
+        }
 
-            $getPedido_complemento = $this->dbc->query("SELECT * FROM factura WHERE idfactura IN ($array_idfacturas)"); //POR COBRAR
+        $factura_comas = implode(",", $array_idfacturas);
 
-            $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 1 AND transacciones_idtransacciones = 0"); //POR COBRAR
+        if($cobrado == 1){ //POR COBRAR
+
+            if($factura_comas == ""){//EL ARRAY ESTABA VACIO
+                 $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 1 AND transacciones_idtransacciones = 0;"); //POR COBRAR
+            }else{
+                $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idfactura IN ($factura_comas)
+                UNION
+                SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 1 AND transacciones_idtransacciones = 0;"); //POR COBRAR
+
+            }
 
         }elseif($cobrado == 2){ // COBRADO
 
-            $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 2 AND transacciones_idtransacciones = 0"); //COBRADO
+            if($factura_comas == ""){//EL ARRAY ESTABA VACIO
+                $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 2 AND transacciones_idtransacciones = 0;"); //POR COBRAR
+           }else{
+               $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idfactura IN ($factura_comas)
+               UNION
+               SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado = 2 AND transacciones_idtransacciones = 0;"); //POR COBRAR
 
+           }
         }else{ //TODOS
-            $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado != 0 AND transacciones_idtransacciones = 0"); //TODOS
+            if($factura_comas == ""){//EL ARRAY ESTABA VACIO
+                $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado != 0 AND transacciones_idtransacciones = 0;"); //POR COBRAR
+           }else{
+               $getPedido = $this->dbc->query("SELECT * FROM factura WHERE idfactura IN ($factura_comas)
+               UNION
+               SELECT * FROM factura WHERE idorganizacion = '$idempresa' AND cobrado != 0 AND transacciones_idtransacciones = 0;"); //POR COBRAR
 
+           }
         }
     
+
         while ($qwe = $this->dbc->fetch($getPedido)) {
             $res = array("id" => $qwe[0], 
             "fecha" => $qwe[1],
@@ -63,6 +91,7 @@ class Filtrado_facturas extends DB{
         }
     
         echo json_encode($lista, JSON_NUMERIC_CHECK);
+        // echo json_encode(array($caja_bancos,$factura_comas));
     }
     public function getidempresa($md5){
         $registro=$this->dbe->query("select * from organizacion where md5(idorganizacion)='$md5'");
