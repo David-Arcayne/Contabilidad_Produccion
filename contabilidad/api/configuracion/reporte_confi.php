163165c3f05f5cac609a7ba9e5d3dcf32c56fe2a
David Arcayne <arcayned6075@gmail.com>
2025-07-28 20:17:24 -0900

    eliminacion reporte confi en proceso

--- contabilidad/api/configuracion/reporte_confi.php
+++ contabilidad/api/configuracion/reporte_confi.php
@@ -798,11 +798,37 @@ class Reporte_confi extends DB{
     }
     public function eliminar_configuracion_reporte($id){
         
-            // $consulta3 = $this->dbc->query("SELECT * FROM vinculacion_cuenta_depreciacion WHERE idvinculacion_cuenta_depreciacion = '$id'");
+            // $consulta3 = $this->dbc->query("SELECT * FROM configuracion_reporte WHERE idconfiguracion_reporte = '$id'");
             // $resultado3 = $consulta3->fetch_assoc();
 
             // $consulta = $this->dbc->query("SELECT COUNT(*) AS total FROM configuracion_reporte WHERE idplandecuenta = '$resultado3[idcuenta]' AND idempresa = '$resultado3[idempresa]'");
             // $resultado33 = $consulta->fetch_assoc(); 
+
+            if (0 > 0) {
+                    $res = array("danger", "No se puede eliminar porque hay registros en proveedor_has_material","eliminar_proveedor");
+            } else {
+                    // Insertar el nuevo registro
+                    $registroProveedor = $this->dbc->query("DELETE FROM configuracion_reporte WHERE idconfiguracion_reporte = '$id'");
+                    if ($registroProveedor === TRUE) {                                                                                                                                                    
+                        $res = array("success", "se elimino exitosamente","eliminarCaracteristica");
+                    } else {
+                        $res = array("danger", "No se pudo registrar");
+                    }
+                }
+                echo json_encode($res);
+    }
+    public function eliminar_configuracion_reporte_activo_fijo($id){
+        
+            $consulta3 = $this->dbc->query("SELECT * FROM configuracion_reporte WHERE idconfiguracion_reporte = '$id'");
+            $resultado3 = $consulta3->fetch_assoc();
+
+            $consulta_vinculacion = $this->dbc->query("SELECT * FROM vinculacion_cuenta_depreciacion WHERE idcuenta = '$resultado3[idplandecuenta]' OR idcuenta_depreciacion = '$resultado3[idplandecuenta]' AND idempresa = '$resultado3[idempresa]'");
+            if($consulta_vinculacion->num_rows > 0){
+
+            }else{
+
+            }
+            // $resultado33 = $consulta_vinculacion->fetch_assoc(); 
 
             if (0 > 0) {
                     $res = array("danger", "No se puede eliminar porque hay registros en proveedor_has_material","eliminar_proveedor");
