60debad740e533f6383f0159af744e1082a77d3f
unknown <arcayned6075@gmail.com>
2025-11-11 09:33:21 -0400

    registro modificadao

--- contabilidad/api/registro.php
+++ contabilidad/api/registro.php
@@ -742,6 +742,6 @@ if($data['ver'] == "asignar_asiento_A_factura") {
     }
 //editar_caja_bancos_facturas editar_caja_bancos_facturas_existentes   editar_recibo_caja_bancos   registrar_factura_recibo_pago_cajaBancos registrar_agrupacion_plantilla registrar_factura_recibo_cobro
 //  vincula  -- crearfacturasf5 registrar_factura_cobro registrar_factura_cobro_otras_cuentas --> aumentar usuario registrar_factura_cobros_tributario registrocobrarfactura registropagarfactura registrar_recibo
-//  registrocobrarfacturaGrupal crearfacturas caja_bancos registrar_factura_pago registrar_otras_cuentas registrar_recibo_otras_cuentas--> aumentar usuario creartransaccion editar_caja_bancos_facturas_existentes 
+//  registrocobrarfacturaGrupal crearfacturas caja_bancos registrar_factura_pago registrar_otras_cuentas registrar_recibo_otras_cuentas--> aumentar usuario creartransaccion editar_caja_bancos_facturas_existentes asignar 
 }// registrar_factura_cobros_tributario registrar_recibo_otras_cuentas registrar_recibo_cobro_cajaBancos_en_otras_cuentas registrar_factura_cobro_otras_cuentas   registrar_otras_cuentas_recibo_cajaBancos_cobro registrar_recibo_cobro
 ?> 
