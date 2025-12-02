85ff679de8efcc3df94b0d88d189ca86ef308448
David Arcayne <arcayned6075@gmail.com>
2025-12-02 00:50:59 -0900

    subir a github ultimo

--- contabilidad/api/consulta.php
+++ contabilidad/api/consulta.php
@@ -72,7 +72,7 @@ $cont=new Transacciones();
 $cont->eliminardetalle($ver[1]);
 }elseif($ver[0]=="reportedetallefpt"){
 $rep=new Reportes();
-$rep->reportedetallefpt($ver[1],$ver[2],$ver[3]);
+$rep->reportedetallefpt($ver[1],$ver[2],$ver[3],$ver[4],$ver[5]);
 }elseif($ver[0]=="reportedetallefptclasefactura"){
     $rep=new Reportes();
     $rep->reportedetallefptclasefactura($ver[1],$ver[2],$ver[3],$ver[4]);
@@ -682,6 +682,6 @@ elseif($ver[0]=="listar_reportes_referencia"){
 
 // reportedetalle listar_recibo_por_id listar_recibo_por_id_otras_cuentas   eliminarcobrados lista_cobrar lista_plande listafactura listaproveedores getgestionactual -- listar_facturas   reportedetallefptclasefactura
 //    listar_recibo_otras_cuentas  listar_recibo_otras_cuentas_pagar       listar_factura           listar_recibo_pago_por_id                                             listar_recibo_por_id   --> caja bancos 
-// reportecomprobantecontable listar_plantilla     listadegestion   getgestion "archivo" => $qwe['archivo'],             listar_recibo_por_id_otras_cuentas      listar_recibo_por_id_otras_cuentas_pagar ---> esos dos son de recibos
+// reportedetallefpt listar_plantilla     listadegestion   getgestion "archivo" => $qwe['archivo'],             listar_recibo_por_id_otras_cuentas      listar_recibo_por_id_otras_cuentas_pagar ---> esos dos son de recibos
 
 ?>
